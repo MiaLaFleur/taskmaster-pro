c3a8aa781154bf3c6bdde33696e2a4e973a8cda2
MiaLaFleur <mia.lafleur@yahoo.com>
2022-03-30 23:23:18 -0500

    fixed drag and drop functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,58 +45,54 @@ var saveTasks = function() {
 
 // make tasks sortable
 $(".card .list-group").sortable({
+  // enable dragging across lists
   connectWith: $(".card .list-group"),
   scroll: false,
   tolerance: "pointer",
   helper: "clone",
-  activate: function(event) {
-    console.log("activate", this);
+  activate: function(event, ui) {
+    console.log(ui);
   },
-  deactivate: function(event) {
-    console.log("deactivate", this);
+  deactivate: function(event, ui) {
+    console.log(ui);
   },
   over: function(event) {
-    console.log("over", event.target);
+    console.log(event);
   },
   out: function(event) {
-    console.log("out", event.target);
+    console.log(event);
   },
-  update: function(event) {
-
-    // array to store the task data in
+  update: function() {
     var tempArr = [];
 
     // loop over current set of children in sortable list
-    $(this).children().each(function() {
-      var text = $(this)
-      .find("p")
-      .text()
-      .trim();
-
-      var date = $(this)
-      .find("span")
-      .text()
-      .trim();
-      
-      
-
-      // add task data to the temp array as an object
-      tempArr.push({
-        text: text,
-        date: date
+    $(this)
+      .children()
+      .each(function() {
+        // save values in temp array
+        tempArr.push({
+          text: $(this)
+            .find("p")
+            .text()
+            .trim(),
+          date: $(this)
+            .find("span")
+            .text()
+            .trim()
+        });
       });
-    });
 
-      // trim down list's ID to match object property
-      var arrName = $(this)
+    // trim down list's ID to match object property
+    var arrName = $(this)
       .attr("id")
       .replace("list-", "");
 
-      // update array on tasks object and save
-      tasks[arrName] = tempArr;
-      saveTasks();
-
-    console.log(tempArr);
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  },
+  stop: function(event) {
+    $(this).removeClass("dropover");
   }
 });
 
